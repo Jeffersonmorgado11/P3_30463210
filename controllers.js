adbdb470adbfceff3104cec1e46d50e2896197a8
Jeffersonmorgado11 <147991144+Jeffersonmorgado11@users.noreply.github.com>
2024-01-11 22:33:00 +0100

    Update controllers.js
    cambiando claves de recaptcha

--- controllers.js
+++ controllers.js
@@ -5,7 +5,7 @@ const path = require('path');
 require('dotenv').config();
 var handlebars = require('express-handlebars')
 const Recaptcha = require('express-recaptcha').RecaptchaV2;
-const recaptcha = new Recaptcha('6LfOBUYpAAAAAN8w1Meatl-oxfr8w4EBhh2Cvsj1', '6LfOBUYpAAAAAO5ERFJseNoPrvc9X0ckgVGdPOkD');
+const recaptcha = new Recaptcha('6LcwIE4pAAAAAJb6xnBbNRxX5VojkotKAJIlRgsu', '6LcwIE4pAAAAAKhRkC0bW_YmnlYXysBgXMLoh99K');
 const multer = require('multer');
 const http = require('http');
 const express = require('express');
