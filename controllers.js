1f265492fc7240613191042d135a0f8b9967a1e9
Jeffersonmorgado11 <147991144+Jeffersonmorgado11@users.noreply.github.com>
2024-02-02 10:22:45 -0400

    Update controllers.js
    captcha

--- controllers.js
+++ controllers.js
@@ -5,8 +5,7 @@ const path = require('path');
 require('dotenv').config();
 var handlebars = require('express-handlebars')
 const Recaptcha = require('express-recaptcha').RecaptchaV2;
-const recaptcha = new Recaptcha('6LcckmQpAAAAANs6HxhzWrEq_nk1ModPLWI42cpX', '6LcckmQpAAAAAL0rNfdXeG4_U9RT5ppblaSSPL-P
-');
+const recaptcha = new Recaptcha('6LcckmQpAAAAANs6HxhzWrEq_nk1ModPLWI42cpX','6LcckmQpAAAAAL0rNfdXeG4_U9RT5ppblaSSPL-P');
 const multer = require('multer');
 const http = require('http');
 const express = require('express');
